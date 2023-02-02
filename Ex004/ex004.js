2d15c56a1e17cc9302d07ae45c1c04b6875c32b3
Gabriel Marcomini <120505807+gabrielmarcomini@users.noreply.github.com>
2023-02-02 20:54:24 -0300

    Praticando

---
+++ Ex004/ex004.js
@@ -0,0 +1,29 @@
+/* var vel = 50.5
+console.log(`a velocidade do carro e de ${vel} km/h`)
+
+if (vel > 70) {
+    console.log(`voce foi multado dirija com cuidado`)
+} else {
+    console.log(`Se beber nao dirija`)
+} */
+
+/* CONDICOES PT2 */
+
+var idade = 67
+if (idade < 16) {
+    console.log('nao vota')
+
+} else if (idade < 18 || idade > 65) {
+    console.log('voto opcional')
+
+} else {
+    console.log('voto normal')
+}
+    
+
+    
+
+
+
+
+    
